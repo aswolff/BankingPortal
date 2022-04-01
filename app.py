094397b7dce271d1fd9fc470558362057c2f7856
David Lee <77169446+dyl20b@users.noreply.github.com>
2022-04-01 14:41:00 -0400

    Added Hashed passwords and Register User
    added hashed passwords for users and added a register user for newer users

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, flash, redirect, url_for
+from flask_session import Session
+from passlib.hash import sha256_crypt
+# pip install passlib
 import sqlite3 as sql
 
-app = Flask(__name__)
+app = Flask(__name__)  # creates flask application
 
 
 @app.route('/')
@@ -14,18 +17,23 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/auth', methods=['POST', 'GET'])
-def auth():
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/auth_user', methods=['POST', 'GET'])
+def auth_user():
     if request.method == 'POST':
         try:
-            email = request.form['Email']
-            password = request.form['Password']
-
             with sql.connect("Bank.db") as con:
+                email = request.form['Email']
                 cur = con.cursor()
-                cur.execute("SELECT * FROM Client WHERE Email = ? AND Password = ?", (email, password))
-                account = cur.fetchone()
-                if account:
+                # connect to database and grab all info under the email input
+                cur.execute("SELECT * FROM Client WHERE Email = ?", [email])
+                data = cur.fetchone()[1]  # this fetches the second value. the password
+                # now we compare the two passwords. the hashed and the input
+                if sha256_crypt.verify(request.form['Password'], data):
                     msg = "success"
                 else:
                     msg = "Incorrect"
@@ -33,8 +41,38 @@ def auth():
             con.rollback()
             msg = "error"
         finally:
-            return render_template("result.html", msg=msg)
             con.close()
+            return render_template("result.html", msg=msg)
+
+
+@app.route('/register_user', methods=['POST', 'GET'])
+def register_user():
+    msg = ""
+
+    if request.method == 'POST':
+        try:
+            email = request.form['Email']
+            password = sha256_crypt.encrypt((str(request.form['Password'])))
+
+            with sql.connect("Bank.db") as con:
+                cur = con.cursor()
+
+                if email == "":
+                    msg = "no empty strings allowed"
+                    raise Exception("no empty strings")
+
+                # if email == existing email
+                # needs implementation
+
+                cur.execute("INSERT INTO Client (Email,Password) VALUES (?,?)", (email, password))
+                con.commit()
+
+                msg = "Account Made"
+        except Exception as e:
+            con.rollback()
+        finally:
+            con.close()
+            return render_template("result.html", msg=msg)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ conn = sqlite3.connect('Bank.db')
 print("Opened database successfully")
 
 # SQLite doesn't impose length restrictions i.e. VARCHAR(255) is ignored and has resulting affinity TEXT
-conn.execute('CREATE TABLE Client (Email TEXT, Password TEXT, PRIMARY KEY(Email))')
+conn.execute('CREATE TABLE Client (Email TEXT, Password TEXT)')
 print("Tables created successfully")
 conn.close()
